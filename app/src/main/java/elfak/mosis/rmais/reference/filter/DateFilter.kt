d4c04cc578e3083f1c4a1fb6df14b41f286f1c4b
ajovano <aleksa.jovanovicc@elfak.rs>
2023-08-07 14:15:53 +0200

    Zaokruzivanje na ceo dan

--- app/src/main/java/elfak/mosis/rmais/reference/filter/DateFilter.kt
+++ app/src/main/java/elfak/mosis/rmais/reference/filter/DateFilter.kt
@@ -5,11 +5,14 @@ import elfak.mosis.rmais.reference.model.ReferencesViewModel
 
 class DateFilter(
     field: String,
-    dateFrom: Long,
-    dateTo: Long,
+    var dateFrom: Long,
+    var dateTo: Long,
     override val referencesViewModel: ReferencesViewModel
 ): IFilter {
     init {
+        val dayInMS: Long = 24 * 3600 * 1000
+        dateFrom = (dateFrom / dayInMS) * dayInMS
+        dateTo = (dateTo / dayInMS) * dayInMS
         val dbRef = ReferenceDB.dbRef.orderByChild(field).startAt(dateFrom.toDouble()).endAt(dateTo.toDouble() + 24 * 3600 * 1000)
         dbRef.addChildEventListener(this)
     }
