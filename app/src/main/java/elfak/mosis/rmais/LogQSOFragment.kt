18b994d3310a69be46407bda4314d6fec3d48cac
ajovano <aleksa.jovanovicc@elfak.rs>
2023-08-07 18:39:18 +0200

    LogQSOFragment layout

---
+++ app/src/main/java/elfak/mosis/rmais/LogQSOFragment.kt
@@ -0,0 +1,17 @@
+package elfak.mosis.rmais
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LogQSOFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log_qso, container, false)
+    }
+}
